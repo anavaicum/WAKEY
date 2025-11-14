0fc5d91d039282d87370845059e9ec9c1b39e1c1
anavaicum <ana.vaicum@stud.ubbcluj.ro>
2025-11-14 15:51:15 +0200

    Initial project setup

---
+++ app/src/main/java/com/wakey/activities/MainActivity.java
@@ -0,0 +1,13 @@
+package com.wakey.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.wakey.R;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
